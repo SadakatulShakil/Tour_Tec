095d2c398bea25cccbb645ec7ad906a9f5ddb9f2
Shakil <sadakatulshakil94@gmail.com>
2020-08-22 17:44:38 +0600

    Home page design fully done including bottom nav, nav drawer, cardView option, auto sliding promo image

--- app/src/main/java/com/example/tourtec/Activity/UserUiContainerActivity.java
+++ app/src/main/java/com/example/tourtec/Activity/UserUiContainerActivity.java
@@ -1,16 +1,83 @@
 package com.example.tourtec.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
+import com.example.tourtec.Fargment.HistoryFragment;
+import com.example.tourtec.Fargment.HomeFragment;
+import com.example.tourtec.Fargment.NearbyPlaceFragment;
+import com.example.tourtec.Fargment.ProfileFragment;
 import com.example.tourtec.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UserUiContainerActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavView;
+    private Toolbar dToolbar;
+    private FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_ui_container);
+
+        initView();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, new HomeFragment())
+                .commit();
+
+        initBottomNavigation();
+
+    }
+
+    private void initBottomNavigation() {
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.homeFm:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nearPlaceFm:
+                        selectedFragment = new NearbyPlaceFragment();
+                        break;
+                    case R.id.historyFm:
+                        selectedFragment = new HistoryFragment();
+                        break;
+                    case R.id.profileFm:
+                        selectedFragment = new ProfileFragment();
+                        break;
+
+                    default:
+                        break;
+                }
+                if (selectedFragment != null) {
+                    FragmentManager fm = getSupportFragmentManager();
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.replace(R.id.fragmentContainer, selectedFragment)
+                            .commit();
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initView() {
+        bottomNavView = findViewById(R.id.bottomNavigationView);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            dToolbar = findViewById(R.id.toolbar);
+        }
+        frameLayout = findViewById(R.id.fragmentContainer);
     }
 }
---
+++ app/src/main/java/com/example/tourtec/Fargment/HomeFragment.java
@@ -0,0 +1,134 @@
+package com.example.tourtec.Fargment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.tourtec.R;
+import com.google.android.material.navigation.NavigationView;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+public class HomeFragment extends Fragment {
+    private CarouselView carouselView;
+    private Context context;
+    private Toolbar dToolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    public int[] sampleImages = {R.drawable.kuyakata, R.drawable.nafakum, R.drawable.panthumai,
+            R.drawable.saintmartin, R.drawable.sajek, R.drawable.sundarban, R.drawable.tanguarhaor,
+            R.drawable.bichanakandi, R.drawable.bogalake, R.drawable.madhobkundo,
+            R.drawable.niladri, R.drawable.panthumai, R.drawable.chandronath};
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initView(view);
+        initNavigationViewDrawer();
+       /* dToolbar.setTitle(getString(R.string.home));*/
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, dToolbar,
+                R.string.drawer_open, R.string.drawer_closed);
+        drawerToggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        carouselView.setImageListener(imageListener);
+        carouselView.setPageCount(sampleImages.length);
+
+    }
+
+    private void initView(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            dToolbar = view.findViewById(R.id.toolbar);
+        }
+        carouselView = view.findViewById(R.id.carouselView);
+        navigationView = view.findViewById(R.id.navigationDrawer);
+        drawerLayout = view.findViewById(R.id.drawer);
+    }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
+    private void initNavigationViewDrawer() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch ((item.getItemId())) {
+
+                    case R.id.settings:
+                       /* getActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frameLayoutID, new SettingsFragment())
+                                .addToBackStack(null)
+                                .commit();*/
+                        Toast.makeText(context, "Settings Under Construction be Patient!", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.feedback:
+                        /*Intent intent = new Intent(context, FeedBackActivity.class);
+                        startActivity(intent);*/
+                        Toast.makeText(context, "FeedBack Under Construction be Patient!", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.adminpanel:
+                        /*Intent intent1 = new Intent(context, AdminActivity.class);
+                        startActivity(intent1);*/
+                        Toast.makeText(context, "Admin Under Construction be Patient!", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.about:
+                       /* Intent intent3 = new Intent(context, AboutUsActivity.class);
+                        startActivity(intent3);*/
+                        Toast.makeText(context, "About Under Construction be Patient!", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.logOut:
+                        /*FirebaseAuth.getInstance().signOut();
+                        getActivity().finish();
+                        Intent intent4 = new Intent(context, SignInActivity.class);
+                        startActivity(intent4);*/
+                        Toast.makeText(context, "LogOut Under Construction be Patient!", Toast.LENGTH_LONG).show();
+                        break;
+
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
+}
