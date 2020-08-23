bc3052bf5d5db52556e9eb64d291321136dbd898
Shakil <sadakatulshakil94@gmail.com>
2020-08-23 13:45:36 +0600

    Home page design fully done including bottom nav, nav drawer, cardView option, auto sliding promo image

---
+++ app/src/main/java/com/example/tourtec/Activity/EventsActivity.java
@@ -0,0 +1,16 @@
+package com.example.tourtec.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tourtec.R;
+
+public class EventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+    }
+}
